1ee56d73ac4878e75493364fb8636890d7f2c9aa
Ralf  Yang <ralf@mz.co.kr>
2019-01-28 11:17:08 +0900

    update test

--- ci/tasks/dev-v0.09_peter.sh
+++ ci/tasks/dev-v0.09_peter.sh
@@ -7,3 +7,4 @@ git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 git remote add -f dev-v0.09_peter ../dev-v0.09_peter
 git merge --no-edit dev-v0.09_peter/dev-v0.09_peter
+
